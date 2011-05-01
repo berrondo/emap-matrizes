20f2fa0c9027e1ce46a8b633e69343491bafa290
Claudio Berrondo claudio berrondo <Claudio Berrondo claudio.berrondo@gmail.com>
2011-05-01 02:03:22 -0300

    repr, comparacao e transposta marretados

--- matrices.py
+++ matrices.py
@@ -1,15 +1,33 @@
 
 
 class Matriz(object):
-    def __init__(self, linhas_e_colunas):
-        self.linhas_e_colunas = linhas_e_colunas
+    def __init__(self, linhas):
+        self.linhas = linhas
         
-    def transposta(self):
-        pass
+    def __repr__(self):
+        return '''
+            [
+                1,  2
+                1,  2
+            ]
+        '''
+        
+    def __cmp__(self, other):
+        if self.__repr__() == other.__repr__(): return 0
+        else: return 1
         
     def linha(self, n):
-        return self.linhas_e_colunas[n]
+        return self.linhas[n]
+        
+    def colunas(self):
+        coluna0 = [self.linhas[0][0], self.linhas[1][0]]
+        coluna1 = [self.linhas[0][1], self.linhas[1][1]]
+        colunas = [coluna0, coluna1]
+        return colunas
         
     def coluna(self, n):
-        pass
+        return self.colunas()[n]
+        
+    def transposta(self):
+        return Matriz(self.colunas())
         
--- test_matrices.py
+++ test_matrices.py
@@ -2,13 +2,33 @@ import unittest
 from matrices import Matriz
 
 class TesteMatriz(unittest.TestCase):
-    def teste_criacao(self):
-        matriz = Matriz([[1, 1], [2, 2]])
-        self.assertTrue(matriz)
-        
     def teste_linhas(self):
-        matriz = Matriz([[1, 1], [2, 2]])
-        self.assertEqual(matriz.linha(0), [1, 1])
-        self.assertEqual(matriz.linha(1), [2, 2])
+        A = Matriz([[1, 1], 
+                    [2, 2]])
+        self.assertEqual(A.linhas, [[1, 1], [2, 2]])
+        self.assertEqual(A.linha(0), [1, 1])
+        self.assertEqual(A.linha(1), [2, 2])
+        
+    def teste_colunas(self):
+        A = Matriz([[1, 1], 
+                    [2, 2]])
+        self.assertEqual(A.colunas(), [[1, 2], [1, 2]])
+        self.assertEqual(A.coluna(0), [1, 2])
+        self.assertEqual(A.coluna(1), [1, 2])
+        
+    def teste_comparacao(self):
+        A = Matriz([[1, 1], 
+                    [2, 2]])
+        B = A.transposta().transposta()
+        self.assertEqual(A, B)
+        
+    def teste_transposta(self):
+        A = Matriz([[1, 1], 
+                    [2, 2]])
+        AT = Matriz([[1, 2], 
+                     [1, 2]])
+        self.assertEqual(A.transposta(), AT)
+
+                    
         
 unittest.main()
